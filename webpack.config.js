03f8361dd37ad62031c8f6c99cea2c354b1a9acc
eligarc <enaiang041294@gmail.com>
2022-10-09 12:22:01 -0500

    :bug: FIX :small_blue_diamond: Webpack configuration error to import react in components and pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 module.exports = {
 	entry: './src/index.js',
@@ -48,6 +49,9 @@ module.exports = {
 		}),
 		new MiniCssExtractPlugin({
 			filename: '[name].css',
+		}),
+		new webpack.ProvidePlugin({
+			React: 'react',
 		}),
 	],
 	devServer: {
